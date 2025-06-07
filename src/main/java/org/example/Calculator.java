7d44fa1589c118b401566107462c645d72ce2947
siddhartha2107 <siddharthayadav202@gmail.com>
2025-06-07 16:18:08 +0530

    updated Calculator.add to support newline delimeter

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,7 +3,7 @@ package org.example;
 public class Calculator {
     public static int add(String numbers) {
         int sum=0;
-        String[] nums = numbers.split(",");
+        String[] nums = numbers.split("[,\n]");
         for(String s:nums){
             sum+=Integer.parseInt(s.trim());
         }
