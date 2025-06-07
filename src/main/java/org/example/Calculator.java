bcda8a502b0c0f2418496ae3ef6d7af042db6690
siddhartha2107 <siddharthayadav202@gmail.com>
2025-06-07 16:43:13 +0530

    Updated the test cases to spport differnt delimeters, updated the code and added some exception handling

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,12 +1,19 @@
 package org.example;
 
 public class Calculator {
-    public static int add(String numbers) {
+    public static int add(String numbers) throws Exception {
         int sum=0;
-        String[] nums = numbers.split("[,\n]");
-        for(String s:nums){
-            sum+=Integer.parseInt(s.trim());
+        if(numbers.startsWith("//")){
+            String delimiter = numbers.substring(2, numbers.indexOf("\n"));
+            String numbersString = numbers.substring(3);
+            String[] nums = numbersString.split(delimiter);
+            for(String s:nums){
+                sum+=Integer.parseInt(s.trim());
+            }
+        } else {
+          throw new IllegalArgumentException("Input string does not starts with //");
         }
+
         return sum;
     }
 }
--- src/test/java/CalculatorTests.java
+++ src/test/java/CalculatorTests.java
@@ -5,12 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTests {
 
     @Test
-    void calculator_basic() {
-        assertEquals(6, Calculator.add("1,2,3"));
+    void calculator_delimiter() throws Exception {
+        assertEquals(6, Calculator.add("//;\n1;2;3"));
     }
 
     @Test
-    void calculator_newline() {
-        assertEquals(6, Calculator.add("1\n2,3"));
+    void calculator_exception() {
+        String invalidInput = "1;2;3";
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Calculator.add(invalidInput);
+        });
+
+        assertTrue(exception.getMessage().contains("Input string does not starts with //"));
     }
+
 }
